d2791ab2d17875dc1210a892ff120e90ebeee246
Salva Juan <salva.juan@proton.me>
2024-05-06 18:27:03 +0200

    solve katas

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,18 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError()
+    if a+b <= c or a+c <= b or b+c <= a:
+        raise TriangleError()
+
+    if a == b and b == c:
+        return 'equilateral'
+    elif a != b and b != c and a != c:
+        return 'scalene'
+    else:
+        return 'isosceles'
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
